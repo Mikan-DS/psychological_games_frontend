fa7e963fec826642466f882c9e620c415771e215
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-07-17 04:37:46 +0300

    Секция 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import AgeImage from "./img/age.png"
 import CoverPics from "./img/Cover_pic_desktop.png"
 // import GogolFont from "./fonts/Gogol Regular.ttf"
 
+import Screenshot1 from "./img/screenshots/1.png"
+import Screenshot2 from "./img/screenshots/2.png"
+import Screenshot3 from "./img/screenshots/3.png"
+import NumberAnimation from "./NumberAnimation";
+
 function App() {
   return (
       <div className="App">
@@ -76,14 +81,86 @@ function App() {
               </div>
 
           </div>
+          <div id="forStudent" style={{maxWidth: "1400px", marginLeft: "auto", marginRight: "auto"}}>
+              <div>
+                  <h1>
+                      ЕСЛИ ТЫ - ШКОЛЬНИК
+                  </h1>
+                  <h2 style={{paddingLeft: "25%"}}>
+                      играй дома, чтобы выигрывать в классе
+                  </h2>
+              </div>
+
+              <div  style={{display: "flex", flexDirection: "row", justifyContent: 'space-between', alignItems: 'flex-start', marginTop: 80, marginBottom: 40}}>
+                  <div className="screenshotsBlock">
+                      <img src={Screenshot1} alt="Скриншот 1"/>
+                      <h4>
+                          Отдохнуть
+                      </h4>
+                      <p>
+                          Погрузись в игру, по жанру это визуальная новелла, основанная на реальных событиях.
+                          Стань Новеньким в этом классе:
+                          найди друзей, решай конфликты и просто получи удовольствие от общения.
+                      </p>
+                  </div>
+                  <div className="screenshotsBlock">
+                      <img src={Screenshot2} alt="Скриншот 2"/>
+                      <h4>
+                          ПОНЯТЬ СЕБЯ
+                      </h4>
+                      <p>
+                          Честно делай выборы в игре, так же,
+                          как поступаешь в реальной жизни – и ты узнаешь свою роль в случае буллинга.
+                          Ты получишь заключение по результат психологическое теста и рекомендации.
+                      </p>
+                  </div>
+                  <div className="screenshotsBlock">
+                      <img src={Screenshot3} alt="Скриншот 3"/>
+                      <h4>
+                          ПРОКАЧАТЬ СЕБЯ
+                      </h4>
+                      <p>
+                          Пройдя игру несколько раз, ты поймешь, как выйти из своей старой роли,
+                          и как своим поведением влиять на одноклассников, меняя отношения в классе.
+                          Немножко изобретательности и смекалки, и можешь смело ходить в школу!
+                      </p>
+                  </div>
+              </div>
+
+              <button style={{background: "none", cursor: "pointer", width: "100%", padding: 38, fontSize: 26, borderRadius: 50}}>
+                  ИГРАТЬ
+              </button>
+
+              <div className="statisticBlock" style={{display: "flex", flexDirection: "row", justifyContent: 'space-between', width:927, marginLeft: "auto", marginTop: 100, marginBottom: 100}}>
+                  <div style={{width:220}}>
+                      <NumberAnimation targetNumber={357} duration={2100}/>
+                      <p>
+                          Варианта развития событий
+                      </p>
+                  </div>
+                  <div style={{width:150}}>
+                      <NumberAnimation targetNumber={6} duration={1500}/>
+                      <p style={{paddingRight: 20}}>
+                          Концовок | ролей
+                      </p>
+                  </div>
+                  <div style={{width:220}}>
+                      <NumberAnimation targetNumber={10} duration={2000}/>
+                      <p>
+                          Прохождений, чтобы открыть весь контент
+                      </p>
+                  </div>
+              </div>
+
+          </div>
           <div style={{height: 2000}}></div>
 
-          <VBox>
-              <h1 id="forStudent">Заголовок школьники</h1>
-              <button>Играть!</button>
-              <button>Играть!</button>
+          {/*<VBox>*/}
+          {/*    <h1 id="forStudent">Заголовок школьники</h1>*/}
+          {/*    <button>Играть!</button>*/}
+          {/*    <button>Играть!</button>*/}
 
-          </VBox>
+          {/*</VBox>*/}
 
           <div>
               Жертвами бубузинга...
