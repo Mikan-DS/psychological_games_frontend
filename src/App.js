5af315d830fc94077d76eb8b62e1800e0935cb3e
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-08-04 18:16:15 +0300

    Fact 3 section full

--- src/App.js
+++ src/App.js
@@ -76,21 +76,25 @@ function App() {
                 initialLogin={initialLogin}/>
             <PolicyModal modalControl={policyModalControl}/>
 
-            {/*<CoverSection user={user} loginModalControl={loginModalControl} playAction={playAction}/>*/}
-            {/*<ForStudent user={user} playAction={playAction}/>*/}
-            {/*<FactSection reverse={false}>*/}
-            {/*    ЖЕРТВАМИ БУЛЛИНГА В РОССИИ СТАЛИ <span>57%</span> ДЕТЕЙ*/}
-            {/*    И <span>70%</span> УЧИТЕЛЕЙ*/}
-            {/*    <div className={"body-text"}>*/}
-            {/*        По статистике за Ноябрь 2023*/}
-            {/*    </div>*/}
-            {/*</FactSection>*/}
-            {/*<ForParent user={user} playAction={playAction}/>*/}
-            {/*<FactSection reverse={true}>*/}
-            {/*    Каждый <span>ВТОРОЙ</span> РЕБЕНОК СТАЛКИВАЕТСЯ С АГРЕССИЕЙ И*/}
-            {/*    КАЖДЫЙ <span>ТРЕТИЙ</span> — С ЕЁ ФИЗИЧЕСКИМИ ПРОЯВЛЕНИЯМИ*/}
-            {/*</FactSection>*/}
+            <CoverSection user={user} loginModalControl={loginModalControl} playAction={playAction}/>
+            <ForStudent user={user} playAction={playAction}/>
+            <FactSection reverse={false}>
+                ЖЕРТВАМИ БУЛЛИНГА В РОССИИ СТАЛИ <span>57%</span> ДЕТЕЙ
+                И <span>70%</span> УЧИТЕЛЕЙ
+                <div className={"body-text"}>
+                    По статистике за Ноябрь 2023
+                </div>
+            </FactSection>
+            <ForParent user={user} playAction={playAction}/>
+            <FactSection reverse={true}>
+                Каждый <span>ВТОРОЙ</span> РЕБЕНОК СТАЛКИВАЕТСЯ С АГРЕССИЕЙ И
+                КАЖДЫЙ <span>ТРЕТИЙ</span> — С ЕЁ ФИЗИЧЕСКИМИ ПРОЯВЛЕНИЯМИ
+            </FactSection>
             <ShockSection/>
+            <FactSection reverse={true}>
+                У <span>ТРЕТИ</span> УЧЕНИКОВ, КОТОРЫХ ТРАВИЛИ В ШКОЛЕ, В БУДУЩЕМ
+                ВОЗНИКАЮТ <span>ПСИХИЧЕСКИЕ РАССТРОЙСТВА</span>
+            </FactSection>
 
             <div style={{height: 500, backgroundColor: "aqua", textAlign: "center"}}>
             </div>
